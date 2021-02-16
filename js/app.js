8ff7a0c73ff0074807118dc758e6025953ebcfdb
Nouf213 <noufshoubaki@gmail.com>
2021-02-16 10:54:09 +0200

    edited the lab

--- js/app.js
+++ js/app.js
@@ -1,56 +1,88 @@
 'use strict';
 
-let loveswim = prompt('Do i love swimming?y/yes','yes').toLowerCase();
-//console.log(loveswim);
-if (loveswim!=='yes') {
-//console.log('wrong answer')
-  alert('wrong answer');
-}
-else{
-//console.log('hell yeaa')
-  alert('hell yeaa');
-}
-
-
-let dive = prompt('Have i ever dive? y/yes','no').toLowerCase();
-//console.log(dive);
-if (dive==='no') {
-  alert('never in my life');
-} else {
-  alert('wrong answer');
-}
-
-let likedive = prompt('Do i like diving?y/yes','no').toLowerCase();
-//console.log(likedive);
-if (likedive!=='no') {
-  alert('yes i dont like diving');
-} else {
-  alert('you dont know me well!!');
-}
-
-let divesuit = prompt('Have i ever wore a diving suit?y/yes','yes').toLowerCase();
-//console.log(divesuit);
-if (divesuit==='no') {
-  alert('haha! tricky question. nooo?');
-} else {
-  alert('comfiest thing ever');
-}
-
-let Coral = prompt('Do i like Coral?y/yes','yes').toLowerCase();
-//console.log(Coral);
-if (Coral!=='yes') {
-  alert('why i would not like them');
-} else {
-  alert('yess, it is full of colors');
-}
-
 let yourname=prompt ('enter your name');
 function printGreeting(nome)
 {
   alert('Welcome'+' '+ nome);
 }
-
 printGreeting (yourname);
+
+let loveswim = prompt('Do i love swimming?y/yes').toLowerCase();
+//console.log(loveswim);
+if ((loveswim!=='yes' || loveswim!=='y') && (loveswim!=='n'|| loveswim!=='no')&&loveswim==='') {
+//console.log('invalid answer')
+
+  alert('invalid answer');
+  loveswim=prompt('Do i love swimming?y/yes');
+}
+else{
+  if(loveswim==='y'|| loveswim==='yes')
+  //console.log('hell yeaa')
+    alert('hell yeaa');
+  else
+  {
+    if(loveswim==='n' || loveswim==='no')
+      alert('wrong answer');
+  }
+}
+
+
+let dive = prompt('Have i ever dive? y/yes').toLowerCase();
+//console.log(dive);
+if ((dive!=='yes' || dive!=='y') && (dive!=='n'|| dive!=='no')||dive==='') {
+  //console.log('invalid answer')
+  alert('invalid answer');
+  dive=prompt('Have i ever dive?y/yes');
+}
+else{
+  if (dive==='no' || dive==='n') {
+    alert('never in my life');
+  } else {
+    if (dive==='yes' || dive ==='y')
+      alert('wrong answer');
+  }
+}
+let likedive = prompt('Do i like diving?y/yes').toLowerCase();
+//console.log(likedive);
+if ((likedive!=='yes' || likedive!=='y') && (likedive!=='n'|| likedive!=='no')|| likedive==='') {
+  //console.log('invalid answer')
+  alert('invalid answer');
+  likedive=prompt('Do i like diving?y/yes');
+}
+if (likedive==='no'|| likedive==='n') {
+  alert('yes i dont like diving');
+} else {
+  if(likedive==='y'||likedive==='yes')
+    alert('you dont know me well!!');
+}
+
+let divesuit = prompt('Have i ever wore a diving suit?y/yes').toLowerCase();
+//console.log(divesuit);
+if ((divesuit!=='yes' || divesuit!=='y') && (divesuit!=='n'|| divesuit!=='no')|| divesuit==='') {
+  //console.log('invalid answer')
+  alert('invalid answer');
+  divesuit=prompt('Have i ever wore a diving suit?y/yes');
+}
+if (divesuit==='no'||divesuit==='n') {
+  alert('haha! tricky question. nooo?');
+} else {
+  if(divesuit==='y'||divesuit==='yes')
+    alert('comfiest thing ever');
+}
+
+let Coral = prompt('Do i like Coral?y/yes').toLowerCase();
+//console.log(Coral);
+if ((Coral!=='yes' || Coral!=='y') && (Coral!=='n'|| Coral!=='no')||Coral==='') {
+  //console.log('invalid answer')
+  alert('invalid answer');
+  Coral=prompt('Do i like Coral?y/yes');
+}
+if (Coral==='no'|| Coral==='n') {
+  alert('why i would not like them');
+} else {
+  if(Coral==='yes'||Coral==='y')
+    alert('yess, it is full of colors');
+}
 
 let Age = prompt('please enter your age?');
 console.log(Age);
@@ -78,4 +110,4 @@ default:
 }
 
 
-printGreeting (yourname);
+printGreeting ('goodbyeee'+' '+yourname);
